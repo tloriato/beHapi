7b48854bc08587c69383b275c6166cefc07f3716
Tiago Loriato <tloriatosimoes@gmail.com>
2018-03-25 12:16:43 -0300

    Route with Params

---
+++ routeToHapiness/main.js
@@ -0,0 +1,23 @@
+const Hapi = require('hapi');
+
+const server = new Hapi.Server({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+function helloStranger(request, h){
+    //console.log(request);
+    return `Hello ${request.params.name}`;
+}
+
+server.route({
+    path: '/{name}',
+    method: 'GET',
+    handler: helloStranger
+});
+
+(async () => {
+    await server.start();
+    console.log(`Server running at ${process.argv[2]}`);
+})();
+
